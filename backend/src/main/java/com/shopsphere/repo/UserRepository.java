12917dc56dd1060e314ba54814e9763cc9c3a359
ravi2001-cell <rabtaravi1069@gmail.com>
2026-09-09 12:12:05 +0530

    Add repositories

---
+++ backend/src/main/java/com/shopsphere/repo/UserRepository.java
@@ -0,0 +1,3 @@
+package com.shopsphere.repo;
+import com.shopsphere.model.User; import org.springframework.data.jpa.repository.JpaRepository; import java.util.Optional;
+public interface UserRepository extends JpaRepository<User,Long>{ Optional<User> findByEmail(String email); boolean existsByEmail(String email); }
